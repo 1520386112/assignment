c9f82422851092229b05f669b7f7e0d658b6ec62
Hurric <=>
2020-04-21 11:20:33 +0800

    2020/4/21

---
+++ 第八章.运算符重载-成员函数.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+class Complex {
+public:
+	Complex() :a(0), b(0) {}
+	Complex(double a, double b) :a(a), b(b) {}
+	void print() {
+		cout << a << "+" << b << "i";
+	}
+	Complex operator+(Complex other) {
+		Complex res;
+		res.a = a + other.a;
+		res.b = b + other.b;
+		return res;
+	}
+	Complex operator-(Complex other) {
+		Complex res;
+		res.a = a - other.a;
+		res.b = b - other.b;
+		return res;
+	}
+	Complex operator+(double other) {
+		Complex res;
+		res.a = a + other;
+		res.b = b;
+		return res;
+	}
+	Complex operator-(double other) {
+		Complex res;
+		res.a = a - other;
+		res.b = b;
+		return res;
+	}
+private:
+	double a;
+	double b;
+};
+
+int  main()
+{
+	Complex  a(3, 4), b(1, -1), c;
+	c = a + b;
+	cout << "a+b=";
+	c.print();
+	cout << endl;
+	c = a - b;
+	cout << "a-b=";
+	c.print();
+	cout << endl;
+	c = a + 100;
+	cout << "a+100=";
+	c.print();
+	cout << endl;
+	c = a - 2.5;
+	cout << "a-2.5=";
+	c.print();
+	cout << endl;
+
+	return  0;
+}
