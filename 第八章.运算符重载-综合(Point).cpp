02cd6145eab8c91eb83afa4e0f5d4e79f020cb29
Hurric <=>
2020-04-21 11:42:50 +0800

    2020/4/21

---
+++ 第八章.运算符重载-综合(Point).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point {
+public:
+	Point(int x, int y):x(x), y(y){}
+	friend ostream& operator<<(ostream &os, Point p) {
+		os << "(" << p.x << "," << p.y << ")" << endl;
+		return os;
+	}
+	Point& operator++() {
+		x++;
+		y++;
+		return *this;
+	}
+	Point operator++(int) {
+		Point ori(0, 0);
+		ori.x = x++;
+		ori.y = y++;
+		return ori;
+	}
+private:
+	int x;
+	int y;
+};
+
+int  main()
+{
+	Point  a(1, 3);
+	cout << a;
+	cout << a++;
+	cout << ++a;
+	return  0;
+}
